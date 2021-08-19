834682f2cc6fdee4cd556e4c26726d655910719f
Alaribe <laribright8@gmail.com>
2021-08-19 01:35:26 -0700

    Add headerItem component

--- components/header.js
+++ components/header.js
@@ -1,9 +1,27 @@
-function header() {
-    return (
-        <header className="">
-            Header component
-        </header>
-    )
+import HeaderItem from "./header-item";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightBulbIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightBulbIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <h2 className="object-contain">Movie App</h2>
+    </header>
+  );
 }
 
-export default header
+export default Header;
