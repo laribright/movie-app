89c368fb06980d910c355d4b18e90be95d3b58b2
Alaribe <laribright8@gmail.com>
2021-08-19 16:11:39 -0700

    Add Tumbnail component in movie component

--- components/movies.js
+++ components/movies.js
@@ -1,5 +1,9 @@
-function Movies() {
-  return <div>Movies</div>;
+import Tumbnail from './tumbnail'
+
+function Movies(props) {
+  const { results } = props;
+
+  return <div>{results.map(result => <Tumbnail />)}</div>;
 }
 
 export default Movies;
