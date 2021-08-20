9697da2f13d0f05161366646a2a9e3f5f7098dc5
Alaribe <laribright8@gmail.com>
2021-08-19 17:10:00 -0700

    Add grid and flex to movies component

--- components/movies.js
+++ components/movies.js
@@ -1,14 +1,15 @@
+import FlipMove from 'react-flip-move';
 import Tumbnail from "./tumbnail";
 
 function Movies(props) {
   const { results } = props;
 
   return (
-    <div>
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
         <Tumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
 }
 
